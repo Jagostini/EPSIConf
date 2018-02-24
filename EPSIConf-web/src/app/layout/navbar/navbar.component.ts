e16b7095f9325a76eb83d786a0e799ac8f174de6
drogo9& <romain.vaidie50@gmail.com>
2018-02-24 17:17:03 +0100

    add(file): add file for navbar and start navbar

---
+++ EPSIConf-web/src/app/layout/navbar/navbar.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'epsiconf-navbar',
+  templateUrl: './navbar.component.html',
+  styleUrls: ['./navbar.component.css']
+})
+export class NavbarComponent {
+}
