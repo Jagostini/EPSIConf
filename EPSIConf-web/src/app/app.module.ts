c7b3e3ab7468d09fa0acd957429f4737e402f135
drogo9& <romain.vaidie50@gmail.com>
2018-02-25 14:06:09 +0100

    add(sidebar): add sidebar component

--- EPSIConf-web/src/app/app.module.ts
+++ EPSIConf-web/src/app/app.module.ts
@@ -4,17 +4,24 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './layout/navbar/navbar.component';
+import { SidebarComponent } from './layout/sidebar/sidebar.component';
+
+//Service
+import { ContactService } from './layout/sidebar/contact.service';
 
 
 @NgModule({
   declarations: [
     AppComponent,
     NavbarComponent,
+    SidebarComponent,
   ],
   imports: [
     BrowserModule
   ],
-  providers: [],
+  providers: [
+    ContactService
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
