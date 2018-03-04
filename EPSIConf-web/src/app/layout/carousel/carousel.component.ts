cea6c0223e704fab1950e2626ae5368a08a47ce4
Jagostini <agostini.j.85@gmail.com>
2018-03-04 22:44:20 +0100

    add component

---
+++ EPSIConf-web/src/app/layout/carousel/carousel.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+    selector: 'epsiconf-carousel',
+    templateUrl: './carousel.component.html',
+    styleUrls: ['./carousel.component.css']
+})
+
+export class CarouselComponent implements OnInit {
+    constructor(
+    ) { }
+
+    ngOnInit() { }
+}
